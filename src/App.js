be3cb0d0fdb70d1aecde9292a981c7885cd9271f
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-02-29 21:06:43 +0530

    Added navbar and landing page!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import LandingPage from './Landingpage/page';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-black bg-gradient-to-tr from-zinc-900/50 to-zinc-700/30">
+      <Navbar />
+      <LandingPage />
+
     </div>
   );
 }
