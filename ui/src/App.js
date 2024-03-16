8ca35a3d2647423e131b881e6129565dbcbe2796
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-03-16 22:24:06 +0530

    Added Landing page!

---
+++ ui/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+// import Navbar from './components/Navbar';
+// import LandingPage from './Landingpage/page';
+import PricingPage from './Pricingpage/page';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black bg-gradient-to-tr from-zinc-900/50 to-zinc-700/30">
+      <Navbar />
+      <LandingPage />
+      {/* <PricingPage /> */}
+    </div>
+  );
+}
+
+export default App;
