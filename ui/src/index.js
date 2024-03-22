cb4db6aa932e370eddd9b6c9b6d5b79daf4ff796
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-03-22 07:59:07 +0530

    Defined routes!

--- ui/src/index.js
+++ ui/src/index.js
@@ -3,11 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import PricingPage from './Pricingpage/page';
+import DeployPage from './DeployPage/page';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/pricing",
+    element: <PricingPage />,
+  },
+  {
+    path: "/deploy",
+    element: <DeployPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
