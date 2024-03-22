d904fefae2e1d4fa12354d4ee402344cedb45b03
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-03-23 00:12:22 +0530

    Created a function to handle requests

---
+++ ui/src/DeployPage/page.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+
+
+function DeployPage() {
+    const [url, setUrl] = useState('');
+    const sendUrl = () => {
+        axios.post("http://localhost:8000/deploy", {url: url}).then((res) => {
+            console.log(res.data.message);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+    const handleUrlChange = (event) => {
+        setUrl(event.target.value);
+    };
+
+    return (
+        <div 
+        className="min-h-screen min-w-screen flex flex-col justify-center items-center
+         bg-black bg-gradient-to-tr from-zinc-900/50 to-zinc-700/30"
+        >
+            <div className="flex w-screen justify-center items-center">
+                <input 
+                    onChange={handleUrlChange}
+                    value={url}
+                    placeholder='Enter the url to your project'
+                    className='rounded-md py-3 px-5 outline-none w-1/5'
+                />
+
+                <button 
+                    onClick={() => sendUrl()}
+                    className='ml-2 bg-black p-3 rounded-md border-[1px] px-6 text-white hover:text-black hover:bg-zinc-200'
+                >
+                    <p className=''>Deploy</p>
+                </button>
+            </div>
+
+        </div>
+    )
+}
+
+export default DeployPage
